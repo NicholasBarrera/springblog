db4c490299a47fd6c0c99b605a605ace8c0f727f
Nicholas Barrera <nickbarerra722@gmail.com>
2022-11-21 09:50:19 -0600

    update

--- src/main/java/com/example/springblog/RollDiceController.java
+++ src/main/java/com/example/springblog/RollDiceController.java
@@ -1,4 +1,34 @@
 package com.example.springblog;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
 public class RollDiceController {
+    @GetMapping("/roll-dice")
+    public String viewRollDice(){
+        return "roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{n}")
+    public String viewResults(@PathVariable int n, Model model){
+
+        int diceRoll = (int) (Math.random() * 6) + 1;
+        String message;
+
+        if(diceRoll == n){
+            message = "You guessed correctly!";
+        }else{
+            message = "Incorrect. Try again";
+        }
+
+        model.addAttribute("n", n);
+        model.addAttribute("diceRoll", diceRoll);
+        model.addAttribute("message", message);
+
+        return "roll-results";
+    }
 }
